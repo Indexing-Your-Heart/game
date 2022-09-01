03ba50168fe3576bc0edf53a54ff2de57208beda
Marquis Kurt <software@marquiskurt.net>
2022-09-01 15:27:30 -0400

    :boom: Refactor template to SwiftUI lifecycle
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ Shared/Views/GameSceneView.swift
@@ -0,0 +1,30 @@
+// 
+//  GameSceneView.swift
+//  Indexing Your Heart
+//
+//  Created by Marquis Kurt on 9/1/22.
+//
+//  This file is part of Indexing Your Heart.
+//
+//  Indexing Your Heart is non-violent software: you can use, redistribute, and/or modify it under the terms of the CNPLv7+
+//  as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Indexing Your Heart comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for
+//  details.
+
+import SpriteKit
+import SwiftUI
+
+struct GameSceneView: View {
+    private var gameScene: SKScene = {
+        let scene = GameScene.newGameScene()
+        // Write any additional logic for setup here.
+        return scene
+    }()
+
+    var body: some View {
+        SpriteView(scene: gameScene, transition: .fade(withDuration: 2))
+            .aspectRatio(16 / 9, contentMode: .fit)
+            .scaledToFill()
+    }
+}
