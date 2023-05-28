a9308d7a5609fff5ce06853fc02fc4405436a127
Marquis Kurt <software@marquiskurt.net>
2023-05-28 11:52:34 -0400

    :sparkles: Add demo menu

---
+++ Shounin/demos/demo_menu.gd
@@ -0,0 +1,14 @@
+extends Window
+
+@onready var stack = $Container/VBoxContainer
+@onready var btn_env = stack.get_node("EnvironmentDemo") as Button
+@onready var btn_prot = stack.get_node("ProtractorDemo") as Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	btn_env.pressed.connect(func():
+		get_tree().change_scene_to_file("res://demos/environment_demo.tscn")
+	)
+	btn_prot.pressed.connect(func():
+		get_tree().change_scene_to_file("res://demos/protractor_demo.tscn")
+	)
