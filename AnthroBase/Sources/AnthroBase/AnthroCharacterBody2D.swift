43b21f42fb82107d874ba0e18819c1f4cdd14e98
Marquis Kurt <software@marquiskurt.net>
2023-05-29 13:56:17 -0400

    :recycle: Replace char script with AnthroBase
    TODO: Why does the get method crash? I suspect it's an internal memory
    issue.

--- AnthroBase/Sources/AnthroBase/AnthroCharacterBody2D.swift
+++ AnthroBase/Sources/AnthroBase/AnthroCharacterBody2D.swift
@@ -18,9 +18,10 @@ public class AnthroCharacterBody2D: CharacterBody2D {
     public var speed: Double
 
     private var movementVector: Vector2 {
-        Vector2(
-            x: Float(Input().getActionStrength(action: "move_right") - Input().getActionStrength(action: "move_left")),
-            y: Float(Input().getActionStrength(action: "move_down") - Input().getActionStrength(action: "move_up")))
+        Input.shared.getVector(negativeX: "move_left",
+                               positiveX: "move_right",
+                               negativeY: "move_up",
+                               positiveY: "move_down")
         .normalized()
     }
  
@@ -38,23 +39,26 @@ public class AnthroCharacterBody2D: CharacterBody2D {
     }
 
     public override func _ready() {
-        sprite = getNode(path: "Sprite") as? Sprite2D
-        animationTree = getNode(path: "Sprite/AnimationTree") as? AnimationTree
-        animationPlayer = getNode(path: "Sprite/AnimationPlayer") as? AnimationPlayer
-        animationState = animationTree?.get(property: "parameters/playback") as? AnimationNodeStateMachinePlayback
+        super._ready()
+        sprite = getNodeOrNull(path: NodePath(stringLiteral: "Sprite")) as? Sprite2D
+        animationTree = getNodeOrNull(path: NodePath(stringLiteral: "Sprite/AnimationTree")) as? AnimationTree
+        animationPlayer = getNodeOrNull(path: NodePath(stringLiteral: "Sprite/AnimationPlayer")) as? AnimationPlayer
+        // FIXME: This causes a crash. Why?
+//        animationState = animationTree?.get(property: StringName("parameters/playback")) as? AnimationNodeStateMachinePlayback
     }
 
-    public override func _physicsProcess(delta _: Double) {
+    public override func _physicsProcess(delta: Double) {
+        if Engine.shared.isEditorHint() { return }
         if movementVector != Vector2.zero {
             updateBlendingProperties(with: movementVector)
             animationState?.travel(toNode: StringName("Walk"))
-            velocity = (velocity * movementVector * Vector2(x: acceleration, y: acceleration))
-                .limitLength(length: speed)
+            velocity = (movementVector * Vector2(x: acceleration, y: acceleration)).limitLength(length: speed)
         } else {
             animationState?.travel(toNode: StringName("Idle"))
             velocity = velocity.moveToward(to: .zero, delta: friction)
         }
         moveAndSlide()
+        super._physicsProcess(delta: delta)
     }
 
 
