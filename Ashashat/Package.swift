2bcdc22dee4a115767f21f7aab4e0e9d5f0ec1bd
Marquis Kurt <software@marquiskurt.net>
2023-10-03 22:35:26 -0400

    :truck: Move AshashatCore to AshashatKit

--- Ashashat/Package.swift
+++ Ashashat/Package.swift
@@ -6,7 +6,6 @@ let package = Package(
     name: "Ashashat",
     platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
-        .library(name: "AshashatCore", targets: ["AshashatCore"]),
         .library(
             name: "Ashashat",
             type: .dynamic,
@@ -14,30 +13,18 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "SwiftGodot", path: "../SwiftGodot"),
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
-        .package(url: "https://gitlab.com/Indexing-Your-Heart/core-dependencies/ConlangKit", branch: "root")
+        .package(url: "https://gitlab.com/Indexing-Your-Heart/core-dependencies/AshashatKit",
+                 from: "1.0.0-DEVELOPMENT-SNAPSHOT-2023-10-03-b")
     ],
     targets: [
         .target(
-            name: "AshashatCore",
-            dependencies: [
-                "ConlangKit",
-                .product(name: "Algorithms", package: "swift-algorithms")
-            ]),
-        .target(
             name: "Ashashat",
-            dependencies: [
-                "AshashatCore",
-                "SwiftGodot",
-                .product(name: "SwiftGodotMacros", package: "SwiftGodot")
-            ],
+            dependencies: ["AshashatKit", "SwiftGodot"],
             swiftSettings: [
                 .unsafeFlags(["-suppress-warnings"])
             ],
             linkerSettings: [
                 .unsafeFlags(["-Xlinker", "-undefined", "-Xlinker", "dynamic_lookup"])
-            ]),
-        .testTarget(name: "AshashatCoreTests",
-                    dependencies: ["AshashatCore"])
+            ])
     ]
 )
