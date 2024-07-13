94ce7025821549a1a0355ea5147718d623096415
Marquis Kurt <software@marquiskurt.net>
2024-07-12 22:50:40 -0400

    Add license header data

--- Packages/Jenson.NET/Class1.cs
+++ Packages/Jenson.NET/Class1.cs
@@ -1,4 +1,21 @@
-﻿using Kadlet;
+﻿#region Copyright
+//
+//  Class1.cs
+//  Indexing Your Heart
+//
+//  Created by Marquis Kurt on 12/7/2024.
+//
+//  This file is part of Indexing Your Heart.
+//
+//  Indexing Your Heart is non-violent software: you can use, redistribute, and/or modify it under the terms of the
+//  CNPLv7+ as found in the LICENSE file in the source code root directory or at
+//  <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Indexing Your Heart comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for
+//  details.
+#endregion
+
+using Kadlet;
 
 namespace Jenson.NET
 {
