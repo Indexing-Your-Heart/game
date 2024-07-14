80ea8afedb82e5b91b317b78eab9959d156d8c14
Marquis Kurt <software@marquiskurt.net>
2024-07-14 15:37:22 -0400

    IOH-45: Add license headers

--- Packages/Jenson.NET.Tests/JensonReaderTests.cs
+++ Packages/Jenson.NET.Tests/JensonReaderTests.cs
@@ -1,4 +1,21 @@
-﻿using Xunit;
+﻿#region Copyright
+//
+//  JensonReaderTests.cs
+//  Indexing Your Heart
+//
+//  Created by Marquis Kurt on 14/7/2024.
+//
+//  This file is part of Indexing Your Heart.
+//
+//  Indexing Your Heart is non-violent software: you can use, redistribute, and/or modify it under the terms of the
+//  CNPLv7+ as found in the LICENSE file in the source code root directory or at
+//  <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Indexing Your Heart comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for
+//  details.
+#endregion
+
+using Xunit;
 using Jenson.NET;
 using Jenson.NET.Models;
 
--- Packages/Jenson.NET/Models/JensonModels.cs
+++ Packages/Jenson.NET/Models/JensonModels.cs
@@ -1,4 +1,21 @@
-﻿namespace Jenson.NET.Models
+﻿#region Copyright
+//
+//  JensonModels.cs
+//  Indexing Your Heart
+//
+//  Created by Marquis Kurt on 14/7/2024.
+//
+//  This file is part of Indexing Your Heart.
+//
+//  Indexing Your Heart is non-violent software: you can use, redistribute, and/or modify it under the terms of the
+//  CNPLv7+ as found in the LICENSE file in the source code root directory or at
+//  <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Indexing Your Heart comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for
+//  details.
+#endregion
+
+namespace Jenson.NET.Models
 {
     public record StoryChapter(int chapterNumber, string title);
     public record Story(string title, string[] authors, StoryChapter? chapter, string? copyright);
